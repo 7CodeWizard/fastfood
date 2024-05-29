42cfed21a9aebb0e14b62478ca71a73fd547a647
Blaze <cookjames0423@gmail.com>
2024-05-29 01:23:19 -0700

    initialize

---
+++ src/components/Favourites.js
@@ -0,0 +1,21 @@
+import Recipe from "./Recipe";
+
+const Favourites = ({ savedItems }) => {
+  return (
+    <div className="favourite-section">
+      {savedItems.length === 0 && (
+        <p className="text-2xl lg:text-4xl font-semibold text-rose-300 text-center pt-10">
+          Favourite list is empty!
+        </p>
+      )}
+
+      <div className="favouties-items-container container mx-auto py-10 flex flex-wrap gap-10 justify-center">
+        {savedItems.map((recipe) => (
+          <Recipe key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Favourites;
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="bg-rose-600 h-20 mx-auto text-center py-3">
+      <h2 className="logo text-2xl font-bold lowercase italic ">
+        Jaron<span className="text-white">Gaston</span>
+      </h2>
+      <p className="font-thin text-white">
+        © all right reserved {new Date().getFullYear()}
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
